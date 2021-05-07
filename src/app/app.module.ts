d49d703951b3743a4df0f044f5608445d60e477f
petekul <just.pete@hotmail.com>
2021-05-07 22:59:42 +0100

    homepage base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { BannerComponent } from './banner/banner.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
+const appRoutes: Routes = [
+  { path: 'hero', component: HeroComponent },
+  { path: 'banner', component: BannerComponent },
+  { path: '**', component: HomeComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    BannerComponent
+    BannerComponent,
+    ToolbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

39375259806da5931ae147e0387eab579a963d7f
petekul <just.pete@hotmail.com>
2021-05-13 17:58:06 +0100

    added images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+// @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
 export class HomeComponent implements OnInit {
 
   title = 'hibkk-website';
@@ -14,3 +16,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
